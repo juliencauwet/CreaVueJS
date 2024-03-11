4ab5697aa52e2daed5ed92672c2647cacc7361dd
moahmed.lahbib <mlhb01@hotmail.com>
2024-03-11 14:05:20 +0100

    nouv  commit

--- src/components/CompMohKing.vue
+++ src/components/CompMohKing.vue
@@ -4,13 +4,16 @@
 </script>
 
 <template>
-
+<div class="diccouleur">
+    <h1>mohaleboss</h1>
+</div>
 
 </template>
 
 
 <style scoped>
-.c2{
+.diccouleur{
+    font-size: 20px;
 background-color: blueviolet;
 margin: 5%;
 }
