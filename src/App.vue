e61fbd66e1972684739933e0251356be09e12ddf
moahmed.lahbib <mlhb01@hotmail.com>
2024-03-11 14:00:27 +0100

    nouv  commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
+import CompMohKing from './components/CompMohKing.vue';
 </script>
 
 <template>
 <Navbar></Navbar>
-  
+   <CompMohKing></CompMohKing>
 </template>
 
 <style scoped>
---
+++ src/components/CompMohKing.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+
+</script>
+
+<template>
+
+
+</template>
+
+
+<style scoped>
+.c2{
+background-color: blueviolet;
+margin: 5%;
+}
+</style>
