601eb51e6d8872a3961fc018b0f33bf6a8f1f2d3
Julien Cauwet <juliencauwet@yahoo.fr>
2024-03-17 19:49:58 +0100

    recherche de tous les ingrédients et de tous les ustensiles

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <script setup>
-import Navbar from './components/Navbar.vue';
-import ComposantdeJu from './components/ComposantdeJu.vue';
-import Popup from "@/components/Popup.vue";
-import ComponentGiovanni from './components/ComponentGiovanni.vue';
-import FaresComp from './components/FaresComponent.vue'
-import CompMohKing from './components/CompMohKing.vue';
+import Navbar from '@/components/Navbar.vue';
+import { recipes } from '@/assets/recipes';
+import RecipeDisplay from './components/RecipeDisplay.vue';
+import { ref } from 'vue';
+
+const allIngredients = ref([]);
+const allUstensils = ref([]);
+
+recipes.map( r=> {
+  r.ingredients.map(i => {
+    allIngredients.value.push(i.ingredient);
+  });
+  r.ustensils.map(u => {
+    allUstensils.value.push(u);
+  })
+})
+
+console.log('avant');
+console.log('longueur de allIngredients', allIngredients.value.length);
+console.log('longueur de allUstensils', allUstensils.value.length);
+
+
+allIngredients.value = [...new Set(allIngredients.value)]
+allUstensils.value = [...new Set(allUstensils.value)]
+
+console.log('après');
+console.log('longueur de allIngredients', allIngredients.value.length);
+console.log('longueur de allUstensils', allUstensils.value.length);
+
+
 </script>
 
 <template>
-<Navbar></Navbar>
-  <Popup nomArticle="Arnaud"></Popup>
-  <ComponentGiovanni/>
-   <CompMohKing></CompMohKing>
-    <FaresComp title="Je suis le composant de ⴼⴰⵔⴻⵙ ⴱⵓⵣⴰⵔⵉⴰ" />
-    <ComposantdeJu></ComposantdeJu>
+  <Navbar></Navbar>
+ 
+  <RecipeDisplay :recipes="recipes"></RecipeDisplay>
+  
+  
 </template>
 <style scoped></style>
