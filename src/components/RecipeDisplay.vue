1e1e5d3fb9c0968a7cb4684c04153b22ee234ac3
Julien Cauwet <juliencauwet@yahoo.fr>
2024-03-17 19:38:45 +0100

    mot-clé recherché dans toutes les parties de la recette

--- src/components/RecipeDisplay.vue
+++ src/components/RecipeDisplay.vue
@@ -9,25 +9,45 @@ filteredRecipes.value = recipes;
 const keyword = ref('');
 
 watch(keyword, () => {
+    const keywordInLowercase = keyword.value.toLowerCase();
     filteredRecipes.value = recipes.filter(r => {
-        return r.description.includes(keyword.value.toLowerCase());
+        return r.description.includes(keywordInLowercase)
+            || r.name.includes(keywordInLowercase)
+            || isKeywordIncludedInIngredientsArray(keywordInLowercase, r.ingredients)
+            || isKeywordIncludedInUstensilsArray(keywordInLowercase, r.ustensils)
+            || r.appliance.includes(keywordInLowercase);
     })
 })
+
+const isKeywordIncludedInIngredientsArray = (word, ingredientsArray) => {
+    ingredientsArray.map(elt => {
+        if(elt.ingredient.includes(word)) {
+            return true;
+        }
+    })
+    return false;
+}
+
+const isKeywordIncludedInUstensilsArray = (word, ustensilsArray) => {
+    ustensilsArray.map(elt => {
+        if (elt.includes(word)) {
+            return true;
+        }
+    })
+    return false;
+}
 
 
 
 </script>
 <template>
-    <h1>{{ keyword }}</h1>
-
+    <h1> Nombre de recettes correspondantes: {{ filteredRecipes.length }}</h1>
     <input class="form-control me-sm-2" type="search" placeholder="Search" v-model="keyword">
     <div class="row">
         <div class="recipe-container col-md-4" v-for="recipe in filteredRecipes">
-            <RecipeView class="recipe"  :recipe="recipe"></RecipeView>
+            <RecipeView class="recipe" :recipe="recipe"></RecipeView>
         </div>
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
