bf0c470a4c9829ccfe9a30312063ad71a6e5885c
Julien Cauwet <juliencauwet@yahoo.fr>
2024-03-18 22:18:29 +0100

    ajout des éléments du store

--- src/stores/myStore.js
+++ src/stores/myStore.js
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia';
 import { recipes } from '@/assets/recipes';
+import { recipeService } from '@/services/recipe-service';
 
 export const useMyStore = defineStore('myStore', {
   state: () => ({
@@ -9,6 +10,9 @@ export const useMyStore = defineStore('myStore', {
 
   },
   actions: {
-    
+    filterRecipes( keyword, ingredients, appliance, ustensils) {
+      this.selectedRecipes = recipes;
+      this.selectedRecipes = recipeService.selectRecipes(this.selectedRecipes, keyword, ingredients, appliance, ustensils);
+    }
   },
 });
