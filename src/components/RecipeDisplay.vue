ac9518fc6e2f3d6fdbef505966976f3e9e5c3f14
Julien Cauwet <juliencauwet@yahoo.fr>
2024-03-21 13:24:30 +0100

    color directive

--- src/components/RecipeDisplay.vue
+++ src/components/RecipeDisplay.vue
@@ -1,12 +1,12 @@
 <script setup>
 import RecipeView from '@/components/RecipeView.vue';
-import { ref } from 'vue';
 import { useMyStore } from '../stores/myStore';
 import { storeToRefs } from 'pinia';
 
 
 const store = useMyStore();
 const { selectedRecipes } = storeToRefs(store);
+const color = 'red'
 
 
 </script>
@@ -14,9 +14,8 @@ const { selectedRecipes } = storeToRefs(store);
     <h1> Nombre de recettes correspondantes: {{ selectedRecipes.length }}</h1>
     <div class="row">
         <div class="recipe-container col-md-4" v-for="recipe in selectedRecipes">
-            <h1> {{ recipe.id }}</h1>
             <router-link :to="`/recipes/${recipe.id}`">
-            <RecipeView class="recipe" :recipe="recipe"></RecipeView>
+            <RecipeView v-color="color" class="recipe" :recipe="recipe"></RecipeView>
         </router-link>
         </div>
     </div>
